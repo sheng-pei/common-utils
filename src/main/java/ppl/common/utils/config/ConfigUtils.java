1b33f8c3e089852d41003908280416f56928ea89
shengpei <fenghe@dtstack.com>
2021-03-18 16:50:43 +0800

    add ConfigUtils.toSet

--- src/main/java/ppl/common/utils/config/ConfigUtils.java
+++ src/main/java/ppl/common/utils/config/ConfigUtils.java
@@ -1,12 +1,15 @@
 package ppl.common.utils.config;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 
 public class ConfigUtils {
+
+    public static <E> Set<E> toSet(Reader reader, Function<Reader, E> convert) {
+        Set<E> res = new HashSet<>();
+        reader.iterator().forEachRemaining(r -> res.add(convert.apply(r)));
+        return res;
+    }
 
     public static <E> List<E> toList(Reader reader, Function<Reader, E> convert) {
         List<E> res = new ArrayList<>();
